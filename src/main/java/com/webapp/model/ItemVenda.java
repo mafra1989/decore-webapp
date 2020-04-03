3f00891e402d4de6923abc3f7beb7744b2ee097b
Evandro Silva <evandro.ms2016@gmail.com>
2020-04-03 03:48:37 -0400

    Importação das vendas

--- src/main/java/com/webapp/model/ItemVenda.java
+++ src/main/java/com/webapp/model/ItemVenda.java
@@ -15,7 +15,7 @@ import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "itens_venda")
+@Table(name = "itens_venda_fix")
 public class ItemVenda implements Serializable {
 
 	private static final long serialVersionUID = 1L;
