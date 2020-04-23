d131669b015054f98de63b2fe84405f063546da4
Evandro Silva <evandro.ms2016@gmail.com>
2020-04-22 22:44:51 -0400

    Fluxo de caixa atualizado, entrada em parcelamentos adicionada e registro de funcionário favorecido em lançamentos.

--- src/main/java/com/webapp/security/Seguranca.java
+++ src/main/java/com/webapp/security/Seguranca.java
@@ -17,22 +17,37 @@ public class Seguranca {
 
 	@Inject
 	private ExternalContext externalContext;
-	
+
 	private byte[] fileContent;
-	
+
 	public String getNomeUsuario() {
 		String nome = null;
-		
+
 		UsuarioSistema usuarioLogado = getUsuarioLogado();
-		
+
 		if (usuarioLogado != null) {
-			nome = usuarioLogado.getUsuario().getNome();	
-                        
-                        if(usuarioLogado.getUsuario().getFoto() != null) {
-			   fileContent = getUsuarioLogado().getUsuario().getFoto();
-		        }
-                }
-		
+			nome = usuarioLogado.getUsuario().getNome();
+
+			if (usuarioLogado.getUsuario().getFoto() != null) {
+				fileContent = getUsuarioLogado().getUsuario().getFoto();
+			}
+		}
+
+		return nome;
+	}
+
+	public String getPrimeiroNome() {
+		String nome = null;
+
+		UsuarioSistema usuarioLogado = getUsuarioLogado();
+
+		if (usuarioLogado != null) {
+			String nomeTemp = usuarioLogado.getUsuario().getNome();
+			String[] dados = nomeTemp.split(" ");
+			
+			nome = dados[0];
+		}
+
 		return nome;
 	}
 
@@ -40,20 +55,17 @@ public class Seguranca {
 	@UsuarioLogado
 	public UsuarioSistema getUsuarioLogado() {
 		UsuarioSistema usuario = null;
-		
-		UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) 
-				FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
-		
+
+		UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) FacesContext
+				.getCurrentInstance().getExternalContext().getUserPrincipal();
+
 		if (auth != null && auth.getPrincipal() != null) {
 			usuario = (UsuarioSistema) auth.getPrincipal();
 		}
-		
+
 		return usuario;
 	}
-	
 
-	
-	
 	public boolean acessoAdministrador() {
 		System.out.println(externalContext.isUserInRole("ADMINISTRADOR"));
 		return externalContext.isUserInRole("ADMINISTRADOR");
@@ -63,27 +75,23 @@ public class Seguranca {
 		System.out.println(externalContext.isUserInRole("USUARIO_AVANCADO"));
 		return externalContext.isUserInRole("USUARIO_AVANCADO");
 	}
-	
+
 	public boolean acessoUsuarioComum() {
 		System.out.println(externalContext.isUserInRole("USUARIO_COMUM"));
 		return externalContext.isUserInRole("USUARIO_COMUM");
 	}
-	
 
-	
-	
 	public String getImageContentsAsBase64() {
-	    return Base64.getEncoder().encodeToString(fileContent);
+		return Base64.getEncoder().encodeToString(fileContent);
 	}
-	
+
 	public byte[] getFileContent() {
-		
-		if(getUsuarioLogado().getUsuario().getFoto() != null) {
+
+		if (getUsuarioLogado().getUsuario().getFoto() != null) {
 			fileContent = getUsuarioLogado().getUsuario().getFoto();
 		}
-		
+
 		return fileContent;
 	}
-	
-	
+
 }
