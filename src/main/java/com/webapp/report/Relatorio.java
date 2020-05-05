f4a8131c12354997623e2d8e3a16262cbe3754fe
Evandro Silva <evandro.ms2016@gmail.com>
2020-05-05 15:48:57 -0400

    Fluxo de caixa atualizado, entrada em parcelamentos adicionada e registro de funcionário favorecido em lançamentos.

--- src/main/java/com/webapp/report/Relatorio.java
+++ src/main/java/com/webapp/report/Relatorio.java
@@ -30,8 +30,13 @@ public class Relatorio<T> {
 			InputStream relatorioStream = this.getClass()
 					.getResourceAsStream("/com/webapp/report/decore-vendas.jasper");
 			
+			System.out.println(relatorioStream);
+			
 			System.out.println(this.getClass()
 					.getResource("/com/webapp/report/decore-vendas.jasper").toString());
+			
+			relatorioStream = context.getExternalContext().getResourceAsStream(
+	                "/com/webapp/report/decore-vendas.jasper");
 			
 			System.out.println(relatorioStream);
 
