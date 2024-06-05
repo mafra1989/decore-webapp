ed85ea205bef5a98137d989680f98340ba529484
Evandro <evandro.mafra1989@gmail.com>
2024-06-04 21:10:29 -0400

    Tratando Null pointer nos dados dos gráficos

--- src/main/java/com/webapp/controller/DashboardBean.java
+++ src/main/java/com/webapp/controller/DashboardBean.java
@@ -2631,8 +2631,12 @@ public class DashboardBean implements Serializable {
 								usuario.getEmpresa());
 						if (listaDeContas.size() == 0) {
 							totalVendasPagasDataValor = (totalVendasPagasDataValor.doubleValue()
-									+ ((BigDecimal) object[2]).doubleValue() + ((BigDecimal) object[4]).doubleValue());
+									+ ((BigDecimal) object[2]).doubleValue());
 								
+							if(object[4] != null) {
+								totalVendasPagasDataValor = totalVendasPagasDataValor.doubleValue() + ((BigDecimal) object[4]).doubleValue();// total desconto
+							}
+							
 							if(object[5] != null) {
 								totalVendasPagasDataValor = totalVendasPagasDataValor.doubleValue() + ((BigDecimal) object[5]).doubleValue();// taxa de entrega
 							}
